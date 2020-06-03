3c520ebe8b082d9ab4ce3d8461685c3e624764a9
DrZanuff <ricardo.machado.nwi@gmail.com>
2020-06-03 17:49:04 -0300

    First Commit

---
+++ Main.gd
@@ -0,0 +1,40 @@
+extends Node
+
+var file_button = load("res://SoundButton/SoundFile.tscn")
+var project_path
+var audio_path
+onready var itens = $Main/Window/Left/Files/Body/Scroll/Itens
+
+func _ready() -> void:
+	project_path = ProjectSettings.globalize_path("res://")
+	audio_path = project_path + "Audios/"
+	
+	var dir = Directory.new()
+	if dir.open(audio_path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir():
+				if str(file_name).find(".import") == -1:
+					var new_file_button = file_button.instance()
+					itens.add_child(new_file_button)
+					new_file_button.set_path(audio_path+file_name)
+					new_file_button.set_name(file_name)
+					new_file_button.get_node("Body/Play/Button").connect("pressed",self,"play_audio",[new_file_button.get_path()])
+			file_name = dir.get_next()
+
+func play_audio(path):
+	$Player.stop()
+	var audio = AudioStreamOGGVorbis.new()
+	var file = File.new()
+	if file.file_exists(path):
+		file.open( path , File.READ )
+		var l = file.get_len()
+		var bytes = file.get_buffer(l)
+		audio.data = bytes
+		$Player.stream = audio
+		$Player.play()
+		file.close()
+	else:
+		printt("no" , path)
+		
---
+++ SoundButton/SoundFile.gd
@@ -0,0 +1,48 @@
+extends MarginContainer
+
+var sound_file = ""
+var sound_name = ""
+
+var play_icon = "res://icons/TransitionImmediateAutoBig.svg"
+var stop_icon = "res://icons/Pause.svg"
+
+func set_path(path : String):
+	sound_file = path
+
+func get_path():
+	return sound_file
+
+func set_name(name : String):
+	sound_name = name
+	$Body/Label.text = name
+
+func get_name():
+	return sound_name
+	
+
+
+func play_audio(status : bool):
+	if status:
+		$Body/Play/Button.icon = load(play_icon)
+	else:
+		$Body/Play/Button.icon = load(stop_icon)
+
+#var sel = false
+#var colors = [
+#	Color(0.588235, 0.588235, 0.588235, 0.298039) ,
+#	Color(0.129412, 0.313726, 0.870588, 0.270588) 
+#	]
+#
+#func _ready() -> void:
+#	$Button.connect( "pressed" , self , "select" ) 
+#
+#
+#func select():
+#	sel = !sel
+#	if sel:
+#		print( $Panel.get("custom_styles/panel") )
+#		$Panel.get("custom_styles/panel").bg_color = colors[1]
+#
+#	else:
+#		$Panel.get("custom_styles/panel").bg_color = colors[0]
+#		print( $Panel.get("custom_styles/panel") )
